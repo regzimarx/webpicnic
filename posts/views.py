09c1d2be51eb0f9a4ac7fd5f30d61b007373fc76
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-04-21 20:29:55 +0800

    Changed form submit redirection

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import AddPostForm
 
 # Create your views here.
 def index(request):
-	posts_list = Post.objects.all()
+	posts_list = Post.objects.filter(published=True)
 	return_data = {'posts':posts_list}
 	return render(request, 'posts/index.html', return_data)
+
+def add_post(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        form.save()
+        return redirect('/account')
+    else:
+        form = AddPostForm()
+        return_data = {'form':form, 'author':request.user.id}
+        return render(request, 'posts/add-post.html', return_data)
