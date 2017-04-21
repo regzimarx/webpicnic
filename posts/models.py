ac930ac18688f35e713d54819a1f5e57ac1963da
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-04-21 20:28:43 +0800

    Added author and published fields in Post model

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
 	title = models.CharField(max_length=250)
 	content = models.TextField()
 	date_created = models.DateTimeField(auto_now_add=True)
+	author = models.PositiveIntegerField()
+	published = models.BooleanField('Published')
 
 	def __str__(self):
 		return "{}".format(self.title)
