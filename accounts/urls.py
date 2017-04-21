42e9def8f0dbd1f2c8ea3a6eff367902bef66849
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-04-21 20:34:21 +0800

    Added logout route

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 urlpatterns = [
     url(r'^register', views.registration, name='registration'),
     url(r'^login', views.login_user, name='login'),
-    url(r'^account', views.account, name='account')
+    url(r'^account', views.account, name='account'),
+    url(r'^logout', views.logout_user, name='logout')
 ]
