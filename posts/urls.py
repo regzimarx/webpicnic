ea11f7c462d64ef6d53b6ce66696a66e7c370ce4
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-04-24 18:34:08 +0800

    Added edit, view blog detail and search post routes

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from posts import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^add-post', views.add_post, name='add_post')
+    url(r'^(?:page/(?P<page>\d+)/)?$', views.index, name="index"),
+    url(r'^add', views.add_post, name='add_post'),
+    url(r'^edit/(?P<id>\d+)', views.edit_post, name='edit_post'),
+    url(r'^(?P<slug>[\w-]+)/$', views.view_post, name='view_post'),
+    url(r'^search', views.search_post, name='search_post')
 ]
