9234f9f13f2e6ed2b222a2d0d1752e7761bc482f
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-04-24 18:33:19 +0800

    Added slug and date_updated fields for posts

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from django.template.defaultfilters import slugify
+from django.conf import settings
 
 from django.db import models
 
@@ -7,8 +9,14 @@ class Post(models.Model):
 	title = models.CharField(max_length=250)
 	content = models.TextField()
 	date_created = models.DateTimeField(auto_now_add=True)
-	author = models.PositiveIntegerField()
+	author = models.ForeignKey(settings.AUTH_USER_MODEL)
 	published = models.BooleanField('Published')
+	slug = models.SlugField(max_length=100)
+	date_updated = models.DateTimeField()
 
 	def __str__(self):
 		return "{}".format(self.title)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Post, self).save(*args, **kwargs)
