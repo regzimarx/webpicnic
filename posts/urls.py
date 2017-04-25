78c7b9696fcefaa2440d06b1ee7297bb41cce8b6
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-04-25 16:57:13 +0800

    Fixed some url regular expressions and some routes are updated to class-based

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from posts import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    url(r'^(?:page/(?P<page>\d+)/)?$', views.index, name="index"),
+    #index page
+    url(r'^(?:page/(?P<page>\d+)/)?$', views.IndexView.as_view(), name='paginated_posts'),
     url(r'^add', views.add_post, name='add_post'),
     url(r'^edit/(?P<id>\d+)', views.edit_post, name='edit_post'),
-    url(r'^(?P<slug>[\w-]+)/$', views.view_post, name='view_post'),
-    url(r'^search', views.search_post, name='search_post')
+    # #/post-slug-here
+    url(r'^(?P<slug>[\w-]+)/$', views.ViewPostView.as_view(), name='view_post'),
+    url(r'^search', views.search_post, name='search_post'),
 ]
